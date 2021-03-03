7830fd2d385956dabcda1ecedd4de62cffa2b4e2
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-03-03 00:23:17 -0600

    Update w1d4e1.js

--- practice/w1d4e1.js
+++ practice/w1d4e1.js
@@ -1,15 +1,15 @@
 /* Write a loop that prints all even number between 1 to 20 (inclusive) • 
 Writebothwhileandforversions */
 
-// let i = 2;
-// while( i<=20){
-//     console.log(i)
-//     i +=2;
-// }
+let i = 2;
+while( i<=20){
+    console.log(i)
+    i +=2;
+}
 
-// for(let i = 2; i <=20; i+=2){
-//     console.log(i)
-// }
+for(let i = 2; i <=20; i+=2){
+    console.log(i)
+}
 
 /*Write a loop that keeps on prompting for age until you enter age older than 18
 • Write both while and do while versions.*/
