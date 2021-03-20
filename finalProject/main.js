84ccaf399a8273efaeaebf2d3649c18c62713ff2
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-03-19 23:57:09 -0500

    update

--- finalProject/main.js
+++ finalProject/main.js
@@ -1,6 +1,10 @@
 function pageLoad(){
     document.getElementById("butBmi").onclick =bmiCalculation;
     document.getElementById("clear").onclick = clear;
+    document.getElementById("del").onclick = delInfo;
+    document.getElementById("butDescription").onclick= report;
+    
+   
     
 }
 
@@ -9,13 +13,19 @@ function bmiCalculation(){
     const weight = document.getElementById("weight").value;
     let bmiVal = Math.round(weight / Math.pow(height,2));
     displayResult(bmiVal);
+    
 
 }
 function clear(){
+    
     document.getElementById("height").value =NaN;
     document.getElementById("weight").value =NaN;
     document.getElementById("spanResult").innerHTML="";
-
+    document.getElementById("report").innerHTML="";
+}
+function delInfo(){
+    document.getElementById("fname").value ="";
+    document.getElementById("lname").value ="";
 }
 
 
@@ -23,35 +33,62 @@ function displayResult(val){
    let resultBmi= document.getElementById("spanResult");
    resultBmi.innerHTML = val;
    if(val < 18.5){
-       colorYellow();
+   resultBmi.className="yellow";
    } else if(val < 24.9){
-       colorGreen();
+    resultBmi.className="green";
    } else if(val < 29.9){
-       colorOrange();
+   resultBmi.className="orange";
    } else {
-       colorRed()
+       resultBmi.className="red";
    }
    }
-   function colorYellow(){
-    let resultBmi= document.getElementById("spanResult");
-    resultBmi.className="yellow";
+//    function colorYellow(){
+//     let resultBmi= document.getElementById("spanResult");
+//     resultBmi.className="yellow";
     
-    }
-    function colorGreen(){
-        let resultBmi= document.getElementById("spanResult");
-        resultBmi.className="green";
+
+    function report(){
+        if(validate()){
+        let val= document.getElementById("spanResult").innerHTML;
+        let firstName = document.getElementById("fname").value;
+        let lastName = document.getElementById("lname").value;
+       let spanReport= document.getElementById("report");
+       spanReport.innerHTML = "Name :- " + firstName.toUpperCase() + " " + lastName.toUpperCase()  +"<br>"+ categories(val) ;
         
+       function categories(val){
+           let cat =  "BMI Category:- "
+        if(val < 18.5){
+            cat +=  "Underweight" +"<br>" + "BMI value range:- Less than 18.5.";
+            spanReport.className="yellow";
+            return cat;
+        } else if(val < 24.9){
+            cat += "Normal"+"<br>" + "BMI Value range:- 18.5 to 24.9";
+            spanReport.className="green";
+            return cat;
+        } else if(val < 29.9){
+           cat += "Overweight" +"<br>" + "BMI value range:- 25 to 29.9";
+           spanReport.className="orange";
+           return cat;
+        } else {
+            cat +="Obese" +"<br>" + "BMI value range:- 30 or greater";
+            spanReport.className="red";
+            return cat;
         }
-        function colorOrange(){
-            let resultBmi= document.getElementById("spanResult");
-            resultBmi.className="orange";
-            
-            }
-            function colorRed(val){
-                let resultBmi= document.getElementById("spanResult");
-                resultBmi.className="red";
-                
-                }
+    }
+    }
+        
+    }
+
+    function validate(){
+        let fname=document.getElementById("fname");
+        let lname= document.getElementById("lname");
+        if(fname.value === "" || lname.value ===""){
+            alert("Personal Information Missing !")
+            return false;
+        }
+        return true;
+    }
+    
     
 
    
